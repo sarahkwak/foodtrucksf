6e46cf5e245565bce7498476990fa3d60f697186
Sarah Kwak <sarahkwak@hotmail.com>
2015-08-19 09:38:53 -0700

    facebook oauth added

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'foods#index'
-
+  match 'auth/:provider/callback', to: 'sessions#create', via: :get
+  match 'auth/failure', to: redirect('/'), via: :get
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: :get
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
