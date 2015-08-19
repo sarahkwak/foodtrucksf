80744cfcad4fafddeb3504fde9a8ae52f82b1df8
Sarah Kwak <sarahkwak@hotmail.com>
2015-08-19 16:32:02 -0700

    fix routhes for oauth

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'foods#index'
-  match 'auth/:provider/callback', to: 'sessions#create', via: :get
-  match 'auth/failure', to: redirect('/'), via: :get
-  match 'signout', to: 'sessions#destroy', as: 'signout', via: :get
+  match 'auth/:provider/callback', to: 'sessions#create', as: 'sign_in', via: :get
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
